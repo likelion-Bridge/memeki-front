a0f97f98ab8bff50742bd5b945a4b99d2cdeeadb
getsoss <getsoss@naver.com>
2023-08-10 19:19:01 +0900

    Feat 밈인포박스

--- src/component/index.jsx
+++ src/component/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Toast } from './emotion/Component';
-import { Header1 } from './detail/Header';
 import { Header } from './emotion/Component';
+import { MemeInfoBox } from './emotion/Component';
 
 export const index = () => {
   return (
     <div>
       index <Toast />
-      <Header1 />
       <Header type="search" />
       <Header type="main" />
+      <MemeInfoBox></MemeInfoBox>
     </div>
   );
 };
