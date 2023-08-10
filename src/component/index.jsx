859e71906029242156413b8eb65ac363ed672fe2
joeunSong <thdwhdms99@naver.com>
2023-08-10 17:00:24 +0900

    Rename - css로 만든 Header 삭제

--- src/component/index.jsx
+++ src/component/index.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Toast } from './emotion/Component';
-import { Header1 } from './detail/Header';
 import { Header } from './emotion/Component';
 
 export const index = () => {
   return (
     <div>
       index <Toast />
-      <Header1 />
       <Header type="search" />
       <Header type="main" />
     </div>
