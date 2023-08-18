f0e2cce5f58daeb3b8b28a33a2228eaf52371b92
joeunSong <thdwhdms99@naver.com>
2023-08-18 21:15:49 +0900

    주석 삭제

--- src/component/emotion/Component.jsx
+++ src/component/emotion/Component.jsx
@@ -246,24 +246,6 @@ export const SearchBar = () => {
  * @param {string} type select목록 (date, country, year)
  */
 export const SelectBox = ({ type, selectClick, selectedOption }) => {
-  // const options = {
-  //   date: [
-  //     { value: 'week', name: '이번 주' },
-  //     { value: 'month', name: '이번 달' },
-  //     { value: 'year', name: '올해' },
-  //   ],
-  //   country: [
-  //     { value: 'korea', name: '국내 밈' },
-  //     // { value: 'foreign', name: '해외 밈' },
-  //     { value: 'japan', name: '일본 밈' },
-  //   ],
-  //   year: [
-  //     { value: '1900', name: '2000년 이전' },
-  //     { value: '2000', name: '2000년 대' },
-  //     { value: '2010', name: '2010년 대' },
-  //     { value: '2020', name: '2020년 대' },
-  //   ],
-  // };
   const styles = {
     date: css`
       width: 10.3rem;
